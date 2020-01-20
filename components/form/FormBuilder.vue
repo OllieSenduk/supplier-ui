f9de76aab898dd8d41be0931b7f3b9ff85208cef
Oliver Senduk <oliver@ichistudios.com>
2020-01-20 14:26:30 +0100

    FEAT // WIP - Header gets added to form

--- components/form/FormBuilder.vue
+++ components/form/FormBuilder.vue
@@ -10,44 +10,7 @@ export default {
     const director = new FormDirector()
 
     director[this.formMethod](builder)
-    // TEMPORARY
-    return h(
-      'div',
-      {
-        class: {
-          'elevation-12': true,
-          'v-card': true,
-          'v-sheet': true,
-          'theme--light': true
-        }
-      },
-      [
-        h(
-          'header',
-          {
-            class: {
-              'v-sheet': true,
-              'v-sheet--tile': true,
-              'theme--dark': true,
-              'v-toolbar': true,
-              'v-toolbar--flat': true,
-              primary: true
-            }
-          },
-          [
-            h(
-              'div',
-              {
-                class: {
-                  'v-toolbar__content': true
-                }
-              },
-              'Login'
-            )
-          ]
-        )
-      ]
-    )
+    return builder.getForm()
   }
 }
 </script>
--- components/form/FormBuilders.js
+++ components/form/FormBuilders.js
@@ -1,7 +1,51 @@
 export default class FormBuilders {
   constructor(h) {
     this.h = h
+    this.fields = []
   }
 
-  setHeader(text) {}
+  setHeader(text) {
+    const render = this.h
+    this.fields.push(
+      render(
+        'header',
+        {
+          class: {
+            'v-sheet': true,
+            'v-sheet--tile': true,
+            'theme--dark': true,
+            'v-toolbar': true,
+            'v-toolbar--flat': true,
+            primary: true
+          }
+        },
+        [
+          render(
+            'div',
+            {
+              class: {
+                'v-toolbar__content': true
+              }
+            },
+            text
+          )
+        ]
+      )
+    )
+  }
+
+  getForm() {
+    return this.h(
+      'div',
+      {
+        class: {
+          'elevation-12': true,
+          'v-card': true,
+          'v-sheet': true,
+          'theme--light': true
+        }
+      },
+      this.fields
+    )
+  }
 }
